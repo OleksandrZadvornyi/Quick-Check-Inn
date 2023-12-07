42265afcc0ad7a658c1b1f9a5f2f54b96bfc99e9
Oleksandr Zadvornyi <zadvornyi.alex16@gmail.com>
2023-12-07 19:08:03 +0200

    Initial commit

---
+++ admin/db.php
@@ -0,0 +1,4 @@
+<?php
+// $con = mysqli_connect("sql208.infinityfree.com", "if0_35570644", "tyeykskfNm43", "if0_35570644_hotel") or die(mysql_error());
+$con = mysqli_connect("localhost", "root", "", "if0_35570644_hotel") or die(mysql_error());
+?>
